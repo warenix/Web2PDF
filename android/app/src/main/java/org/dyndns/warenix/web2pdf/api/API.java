dbc4fd15dd27c501dda9c7f53b3c5cf39e4db070
warenix <warenix@gmail.com>
2015-12-27 01:57:02 +0800

    migrate service to sailabove.io

--- android/app/src/main/java/org/dyndns/warenix/web2pdf/api/API.java
+++ android/app/src/main/java/org/dyndns/warenix/web2pdf/api/API.java
@@ -1,12 +1,58 @@
 package org.dyndns.warenix.web2pdf.api;
 
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 /**
  * Created by warenix on 3/21/15.
  */
 public class API {
-    public static Result makeCall(Request request) throws IOException {
+    static final String URL = "http://web2pdf.warenix.app.sailabove.io:5000/pdf/convert";
+
+    public static Result makeCall(Pdf.ConvertService request) throws IOException {
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost(URL);
+
+        StringEntity se = new StringEntity(request.toString());
+        se.setContentType("application/json;charset=UTF-8");
+        se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json;charset=UTF-8"));
+        httppost.setEntity(se);
+        HttpResponse httpresponse = httpclient.execute(httppost);
+        String responseText = null;
+        try {
+            responseText = EntityUtils.toString(httpresponse.getEntity());
+            if (responseText != null) {
+                try {
+                    JSONObject json = new JSONObject(responseText);
+                    JSONObject resultJson = json.getJSONObject("result");
+                    Pdf.ConvertResult result = new Pdf.ConvertResult();
+                    result.result = new Pdf.ConvertResult.Result();
+                    result.result.pdf_url = resultJson.getString("pdf_url");
+                    result.result.url = resultJson.getString("url");
+                    return result;
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+            Log.i("Parse Exception", e + "");
+        }
+
         return null;
 
     }
--- android/app/src/main/java/org/dyndns/warenix/web2pdf/api/Pdf.java
+++ android/app/src/main/java/org/dyndns/warenix/web2pdf/api/Pdf.java
@@ -1,5 +1,8 @@
 package org.dyndns.warenix.web2pdf.api;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by warenix on 3/21/15.
  */
@@ -8,7 +11,6 @@ public class Pdf {
 
     public static enum PageSize {
         A3, A4, LEGAL, LETTER,
-
     }
 
     public static enum Orientation {
@@ -25,6 +27,35 @@ public class Pdf {
             mPageSize = pageSize;
             mOrientation = oirentation;
         }
+
+        @Override
+        public String toString() {
+            JSONObject json = new JSONObject();
+            if (mPageSize != null) {
+                try {
+                    json.put("page-size", pageSizeToString(mPageSize));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (mOrientation != null) {
+                try {
+                    json.put("orientation", orientationToString(mOrientation));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (mUrl != null) {
+                try {
+                    json.put("url", mUrl);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            return json.toString();
+        }
+
+
     }
 
     public static class ConvertResult implements API.Result {
@@ -40,5 +71,30 @@ public class Pdf {
             public String url;
             public String pdf_url;
         }
+    }
+
+    public static String pageSizeToString(PageSize pageSize) {
+        switch (pageSize) {
+            case A3:
+                return "A3";
+            case A4:
+                return "A4";
+            case LEGAL:
+                return "Legal";
+            case LETTER:
+                return "Letter";
+        }
+        return "";
+    }
+
+    public static String orientationToString(Orientation orientation) {
+        switch (orientation) {
+
+            case PORTRAIT:
+                return "Portrait";
+            case LANDSCAPE:
+                return "Landscape";
+        }
+        return "";
     }
 }
